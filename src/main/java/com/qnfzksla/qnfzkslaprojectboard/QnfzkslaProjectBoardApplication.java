077b592864a440bd76fa22a38186211fcd2fe402
sd <p971205@naver.com>
2023-01-25 16:18:49 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 엑츄에이터
    * 데브틀즈
    * 롬복

---
+++ src/main/java/com/qnfzksla/qnfzkslaprojectboard/QnfzkslaProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.qnfzksla.qnfzkslaprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnfzkslaProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnfzkslaProjectBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qnfzksla/qnfzkslaprojectboard/QnfzkslaProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnfzksla.qnfzkslaprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnfzkslaProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
