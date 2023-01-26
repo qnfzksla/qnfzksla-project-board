319b5c03c9c083d828e37735b272f47a00c79447
sd <p971205@naver.com>
2023-01-26 17:16:31 +0900

    #10 - 테스트 파일 오류나 수정

--- src/test/java/com/qnfzksla/qnfzkslaprojectboard/QnfzkslaProjectBoardApplicationTest.java
+++ src/test/java/com/qnfzksla/qnfzkslaprojectboard/QnfzkslaProjectBoardApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QnfzkslaProjectBoardApplicationTests {
+public class QnfzkslaProjectBoardApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
 
+    @Test
+    void contextLoads() {
+    }
 }
