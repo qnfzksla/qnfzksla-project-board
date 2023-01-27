0bac33940a934f7d352180785a25eed991ccd565
sd <p971205@naver.com>
2023-01-28 08:05:20 +0900

    #16 - 게시판 뷰 기본 골격 작성
    뷰 적절히 뺘대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이
    들어갈 수 있음을 확인했기 때문에 
    적절히 테스트 메소드 수정

--- src/test/java/com/qnfzksla/qnfzkslaprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/qnfzksla/qnfzkslaprojectboard/controller/ArticleControllerTest.java
@@ -24,7 +24,7 @@ class ArticleControllerTest {
             this.mvc = mvc;
       }
 
-      @Disabled("구현 중")
+//      @Disabled("구현 중")
       @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
       @Test
      public  void givenNothing_whenRequestingArticlesView_then() throws Exception {
@@ -34,7 +34,7 @@ class ArticleControllerTest {
             // when & then
             mvc.perform(get("/articles"))
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.TEXT_HTML))
+                    .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                     .andExpect(view().name("articles/index"))
                     .andExpect(model().attributeExists("articles"));
      }
