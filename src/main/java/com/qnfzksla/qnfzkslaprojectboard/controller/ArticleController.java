48862d2a0dc1262ec12c80be01a40981c8f86195
sd <p971205@naver.com>
2023-02-04 18:52:23 +0900

    #28 - 페이지네이션 기능을 컨트롤러(게시판 뷰) 에 반영
    테스트 'Page' 클래스를 이용하기 때문에
    하는김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현에서 이어서 활용할 전

--- src/main/java/com/qnfzksla/qnfzkslaprojectboard/controller/ArticleController.java
+++ src/main/java/com/qnfzksla/qnfzkslaprojectboard/controller/ArticleController.java
@@ -4,8 +4,10 @@ import com.qnfzksla.qnfzkslaprojectboard.domain.type.SearchType;
 import com.qnfzksla.qnfzkslaprojectboard.response.ArticleResponse;
 import com.qnfzksla.qnfzkslaprojectboard.response.ArticleWithCommentResponse;
 import com.qnfzksla.qnfzkslaprojectboard.service.ArticleService;
+import com.qnfzksla.qnfzkslaprojectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -24,13 +27,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArticleController {
 
     private  final ArticleService articleService;
+    private  final PaginationService paginationService;
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10,sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            ModelMap map){
-        map.addAttribute("articles",articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+            ModelMap map
+    ){
+       Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
