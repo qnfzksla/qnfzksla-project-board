5af4a43a8bd63046cddb9c531caf964b50aa4c67
sd <p971205@naver.com>
2023-02-23 08:45:42 +0900

    #39 - 추가 도메인 설계

---
+++ src/main/java/com/fastcampus/projectboard/repository/querydsl/HashtagRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.fastcampus.projectboard.repository.querydsl;
+
+import java.util.List;
+
+public interface HashtagRepositoryCustom {
+
+    List<String> findAllHashtagNames();
+
+
+}
--- src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTest.java
+++ src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTest.java
@@ -3,11 +3,12 @@ package com.fastcampus.projectboard;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
-
+@ActiveProfiles("test")
 @SpringBootTest
-public class QnfzkslaProjectBoardApplicationTest {
+public class FastCampusProjectBoardApplicationTest {
 
 
     @Test
