45d88e04fba571a5545ab40ca310d411a95e4bb9
sd <p971205@naver.com>
2023-03-06 21:24:59 +0900

    #78- swagger UI 로 문서화

--- src/main/java/com/fastcampus/projectboard/controller/MainController.java
+++ src/main/java/com/fastcampus/projectboard/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.dto.response.ArticleCommentResponse;
+import com.fastcampus.projectboard.dto.response.ArticleWithCommentsResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class MainController {
@@ -9,5 +13,17 @@ public class MainController {
     @GetMapping("/")
     public  String root(){
         return  "forward:/articles";
+    }
+
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(){
+        return ArticleCommentResponse.of(
+                1L,
+                "content",
+                LocalDateTime.now(),
+                "e@mail.com",
+                "Uno",
+                "uno"
+        );
     }
 }
